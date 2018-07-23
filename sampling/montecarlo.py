6fc4fb2ed4705eb8aa12f49849b57dd492182cc5
Soheil Hajian <soheil.hajian@outlook.com>
2018-07-23 11:01:44 +0200

    montecarlo main is changed

--- sampling/montecarlo.py
+++ sampling/montecarlo.py
@@ -78,35 +78,4 @@ class MonteCarlo(object):
 
 
 if __name__=="__main__":
-
-    from random import uniform
-    
-    def sampling_func():
-        x = uniform(0.0, 1.0)   
-        y = uniform(0.0, 1.0)
-        
-        if (x**2 + y**2 <= 1):
-            return True
-        else:
-            return False
-        
-    def aggregate_func(samples):
-            
-        count_true  = 0
-        count_total = len(samples)
-    
-        for sample in samples:
-            if sample == True:
-                count_true += 1
-
-        return (4.0 * count_true / count_total)
-
-
-    
-    num_samples = 10000000
-    n_jobs = 4
-
-    mc = MonteCarlo(sampling_func, aggregate_func)
-    pi = mc.simulate(num_samples, n_jobs)
-
-    print ("pi: {}".format(pi))
+    pass
