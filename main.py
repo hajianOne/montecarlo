8f6c9d0b6ae9d8a28d01cb8bb622e12ff5d3f961
Soheil Hajian <soheil.hajian@outlook.com>
2018-07-24 13:40:35 +0200

    main file improved: adding graphics

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def sampling_func():
     y = uniform(0.0, 1.0)
 
     if (x**2 + y**2 <= 1):
-        return True
+        return (x, y, True)
     else:
-        return False
+        return (x, y, False)
 
 def aggregate_func(samples):
 
@@ -18,17 +18,41 @@ def aggregate_func(samples):
     count_total = len(samples)
 
     for sample in samples:
-        if sample == True:
+        _, _, status = sample
+        if status == True:
             count_true += 1
 
-    return (4.0 * count_true / count_total)
+    return {
+        "pi": (4.0 * count_true / count_total),
+        "true": [(x,y) for (x,y,status) in samples if status==True],
+        "false": [(x,y) for (x,y,status) in samples if status==False]
+        }
 
 
 
-num_samples = 20000000
-n_jobs = 2
+def simulate():
+    num_samples = 10000
+    n_jobs = 2
 
-mc = MonteCarlo(sampling_func, aggregate_func)
-pi = mc.simulate(num_samples, n_jobs)
+    mc = MonteCarlo(sampling_func, aggregate_func)
+    result = mc.simulate(num_samples, n_jobs)
+    return result
 
-print ("pi: {}".format(pi))
+# print ("pi: {}".format(pi))
+
+if __name__=="__main__":
+    import matplotlib.pyplot as plt
+    result = simulate()
+
+    true = {"x": [x for (x,y) in result["true"]], 
+            "y": [y for (x,y) in result["true"]], 
+    }
+
+    false = {"x": [x for (x,y) in result["false"]], 
+             "y": [y for (x,y) in result["false"]], 
+    }
+    plt.plot(true["x"], true["y"], "o")
+    plt.plot(false["x"], false["y"], "o")
+    plt.title("Pi ~ {}".format(result["pi"]))
+    plt.show()
+
